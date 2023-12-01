26b97171421627ff8f5a1046a94aabc65d28eccd
Lincoln Berto dos Santos <lincoln.santospro@gmail.com>
2023-12-01 09:06:27 -0300

    adicionando aula 05

---
+++ modulo_05/aula_04/index.php
@@ -0,0 +1,20 @@
+<?php
+	/*String*/
+	$nome = 'Berto';
+
+	/*Número ou inteiro*/
+	$idade = 25;
+
+	/*Boolean*/
+	$bool = true;
+
+	/*Double*/
+	$double = 10.569;
+
+
+	/*Agora vou printar na tela minhas duas variáveis*/
+	echo 'Meu nome é '.$nome.'<br/>';
+	echo 'Minha idade '.$idade.' anos <br/>';
+	echo $bool.'<br/>';
+	echo $double
+?>
