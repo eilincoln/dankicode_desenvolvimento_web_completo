368dbd86632f02ca43af1bba41ebbf38eccb0418
Lincoln Berto dos Santos <lincoln.santospro@gmail.com>
2023-12-01 08:42:57 -0300

    adicionando módulo 5 e primeira aula de PHP

---
+++ modulo_05/aula_01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Minha Página de Teste</title>
+</head>
+<body>
+	<p>Olá Mundo</p>
+	<?php
+		echo "Olá Mundo! PHP";
+	?>
+</body>
+</html>
