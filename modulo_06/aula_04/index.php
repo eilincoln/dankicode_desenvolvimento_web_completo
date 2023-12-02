56b51ac7308c835bdeed1630202ae40c9710ee03
Lincoln Berto dos Santos <lincoln.santospro@gmail.com>
2023-12-02 18:46:18 -0300

    adicionando informações sobre functions

---
+++ modulo_06/aula_04/index.php
@@ -0,0 +1,26 @@
+<?php  
+
+	/*
+
+	function mostrarNome($nome, $idade){
+		echo '<h2>Nome é: </h2>'.$nome;
+		echo '<br><hr/>';
+		echo "Idade: $idade";
+	}
+	mostrarNome('Claudio', 25);
+
+	*/
+
+
+
+	function calculadora($numero1 = 10, $numero2 = 10){
+		echo ($numero1 + $numero2);
+	}
+
+	calculadora(20, 40);
+
+
+
+	
+
+?>
