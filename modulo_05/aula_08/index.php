2a3fcd1799bf4826c1453ac4b4ceecead90a26b7
Lincoln Berto dos Santos <lincoln.santospro@gmail.com>
2023-12-02 15:31:29 -0300

    adicionando anotações da aula 08

---
+++ modulo_05/aula_08/index.php
@@ -0,0 +1,17 @@
+<?php
+
+	$numero = 5 + 5;
+	$numero2 = 100;
+
+	$resultado = $numero + $numero2 - 2;
+
+	// echo $resultado;
+
+
+	// importante lembrar que aqui existe a precedência também. É bom tomar cuidado. 
+	$expressao = (10 + 20) + 30 / 50;
+
+	echo $expressao 
+
+
+?>
