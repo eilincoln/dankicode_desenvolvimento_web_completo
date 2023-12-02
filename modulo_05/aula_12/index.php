fb84e4dc3a81f2c8c2a859c12d329200f2b46dc8
Lincoln Berto dos Santos <lincoln.santospro@gmail.com>
2023-12-02 17:10:35 -0300

    adicionando anotações sobre aula 12

---
+++ modulo_05/aula_12/index.php
@@ -0,0 +1,27 @@
+<?php  
+
+		/*
+
+	//Laço de repetição for
+	for($contador = 1;$contador <= 10; $contador++) {
+		echo 'hello';
+		echo '<br />';
+		echo $contador;
+		echo '<hr>';
+	}
+
+		*/
+
+
+		/*
+	//Laço de repetição while
+	$contador = 0;
+	while($contador <= 10){
+		echo $contador;
+		echo ' ola mundo';
+		echo '<br/>';
+		$contador++;
+	}
+		*/
+
+?>
