fd40c7f57fb07db972c7e504b8ac32863e60c9b2
Lincoln Berto dos Santos <lincoln.santospro@gmail.com>
2023-12-02 15:22:28 -0300

    adicionando exercicio 07

--- modulo_05/aula_06/index.php
+++ modulo_05/aula_06/index.php
@@ -23,5 +23,4 @@
 	echo '<br/>';
 	echo $informacao['cidade'];
 
-
 ?>
---
+++ modulo_05/aula_07/index.php
@@ -0,0 +1,13 @@
+<?php
+	
+	$nome = "Lincoln";
+	$idade = 23;
+
+	define('NOME', 'Kamilly');
+
+	//nova forma de fazer!
+	echo "Meu nome é $nome";
+
+	
+
+?>
