7f2f9b6065610b214be1257f6a5f5ba270f72be8
Lincoln Berto dos Santos <lincoln.santospro@gmail.com>
2023-12-02 15:43:24 -0300

    adicionando anotações da aula 09

---
+++ modulo_05/aula_09/index.php
@@ -0,0 +1,12 @@
+<?php
+	
+	$nome1 = 'Lincoln';
+	$nome2 = 'Kamilly';
+
+	if($nome1 == $nome2){
+		echo '<div style="width:300px;height:300px;background:red;"></div>';
+	} else {
+		echo 'A condição não bate';
+	}
+
+?>
